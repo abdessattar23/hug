99eaad924cd0d8c86605f7047deaf3b07580de7b
Abdessattar Elyagoubi <106479409+abdessattar23@users.noreply.github.com>
2024-05-22 17:21:11 +0100

    Update server.py

--- server.py
+++ server.py
@@ -9,13 +9,13 @@ def receive():
   chatbot = hugchat.ChatBot(cookie_path="/something/")
   d = request.json
   query = d.get('prompt')
-  resp = chatbot.query(query)
+  resp = chatbot.chat(query).wait_until_done().strip()
   return resp
 @app.route('/chat', methods=['GET'])
 def normal():
   chatbot = hugchat.ChatBot(cookie_path="/something/")
   query = request.args.get('prompt')
-  resp = chatbot.query(query)
+  resp = chatbot.chat(query).wait_until_done().strip()
   return resp
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
